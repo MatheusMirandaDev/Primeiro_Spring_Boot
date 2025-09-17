3aa0f4c72bd6142dcb0c26713fb36d5dd3b381bd
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-16 21:30:47 -0300

    feat: adicionar campo id no DTO

--- src/main/java/med/voll/api/medico/DadosListagemMedicosDTO.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedicosDTO.java
@@ -1,7 +1,7 @@
 package med.voll.api.medico;
 
-public record DadosListagemMedicosDTO (String nome, String email, String crm, Especialidade especialidade){
+public record DadosListagemMedicosDTO (Long id, String nome, String email, String crm, Especialidade especialidade){
     public  DadosListagemMedicosDTO (Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
