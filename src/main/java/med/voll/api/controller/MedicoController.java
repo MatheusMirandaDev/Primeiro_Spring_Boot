bf5df125f45234e3b2dd73d6c48242a4131081ae
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-16 21:00:41 -0300

    feat: adicionar paginação ao endpoint de listagem de médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,9 +7,9 @@ import med.voll.api.medico.DadosListagemMedicosDTO;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -26,9 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedicosDTO> listar(){
-        return repository.findAll().stream().map(DadosListagemMedicosDTO::new).toList();
+    public Page<DadosListagemMedicosDTO> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicosDTO::new);
     }
-
-
 }
