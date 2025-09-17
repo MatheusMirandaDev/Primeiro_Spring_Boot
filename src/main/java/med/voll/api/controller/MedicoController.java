472667f314770c006c888e54d3efa9f5f62cf4aa
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-16 22:34:50 -0300

    feat: implementar delete lógico, marcando médico como inativo

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -38,6 +38,7 @@ public class MedicoController {
     @DeleteMapping("/{id}")
     @Transactional
     public void excluir(@PathVariable Long id){
-        repository.deleteById(id);
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
