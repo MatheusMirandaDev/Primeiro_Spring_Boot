d05caf9f678421a61503399a2c6f61a2f231db78
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-16 21:22:20 -0300

    feat: adicionar paginação e ordenação crescente por nome

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,7 +27,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedicosDTO> listar(Pageable paginacao){
+    public Page<DadosListagemMedicosDTO> listar(@PageableDefault(size = 10, sort = {"nome"} ) Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedicosDTO::new);
     }
 }
