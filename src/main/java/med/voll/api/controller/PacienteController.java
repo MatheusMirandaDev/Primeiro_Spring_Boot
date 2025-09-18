5e10e5c36a4bbfe867d4064a3f0644e895acde90
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-18 19:18:29 -0300

    feat: criar endpoint GET para listagem de pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -3,13 +3,14 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPacienteDTO;
+import med.voll.api.paciente.DadosListagemPacienteDTO;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -22,6 +23,10 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPacienteDTO dados){
         repository.save(new Paciente(dados));
+    }
 
+    @GetMapping
+    public Page<DadosListagemPacienteDTO> listar(@PageableDefault(size = 10, sort = {"nome"} ) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPacienteDTO::new);
     }
 }
