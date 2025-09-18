ff1cbda9d9e9bbb3fd82437755518eb9338fc635
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-18 20:46:45 -0300

    feat: criar DadosDetalhamentoMedicoDTO para detalhar retorno da API

---
+++ src/main/java/med/voll/api/medico/DadosDetalhamentoMedicoDTO.java
@@ -0,0 +1,9 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedicoDTO(Long id, String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco) {
+    public DadosDetalhamentoMedicoDTO(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
+    }
+}
