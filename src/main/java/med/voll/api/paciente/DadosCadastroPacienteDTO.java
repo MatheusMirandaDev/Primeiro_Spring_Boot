9830eb00f71c9d8c09571681288d8d1c497ba49c
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-17 21:19:07 -0300

    fix: corrigir pattern do CPF no DadosCadastroPacienteDTO

--- src/main/java/med/voll/api/paciente/DadosCadastroPacienteDTO.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPacienteDTO.java
@@ -17,7 +17,7 @@ public record DadosCadastroPacienteDTO (
         String telefone,
 
         @NotBlank
-        @Pattern(regexp = "\\d{11}")
+        @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}")
         String cpf,
 
         @NotNull
