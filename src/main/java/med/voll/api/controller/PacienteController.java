dc73553da0a3990ddd75b99be03e33d154598e81
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-18 19:50:55 -0300

    feat: adicionar endpoints PUT e DELETE no PacienteController

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPacienteDTO;
-import med.voll.api.paciente.DadosListagemPacienteDTO;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,4 +26,19 @@ public class PacienteController {
     public Page<DadosListagemPacienteDTO> listar(@PageableDefault(size = 10, sort = {"nome"} ) Pageable paginacao){
         return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPacienteDTO::new);
     }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPacienteDTO dados){
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var paciente = repository.getReferenceById(id);
+        paciente.excluir();
+    }
+
 }
