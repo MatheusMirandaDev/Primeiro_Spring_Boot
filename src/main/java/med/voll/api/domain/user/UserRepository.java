730704921465601c51c2364add180985f48e7e62
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-23 20:02:24 -0300

    feat: criar UserRepository com método findByLogin

--- src/main/java/med/voll/api/domain/user/UserRepository.java
+++ src/main/java/med/voll/api/domain/user/UserRepository.java
@@ -1,4 +1,8 @@
 package med.voll.api.domain.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
 }
