608d388225109e74c9b2edc07ae7f8df55e1aaa8
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-23 20:00:11 -0300

    feat: criar tabela users com migration V6

--- src/main/resources/db/migration/V6__create-table-users.sql
+++ src/main/resources/db/migration/V6__create-table-users.sql
@@ -1,14 +1,5 @@
-CREATE TABLE medicos (
+CREATE TABLE users (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    crm VARCHAR(6) NOT NULL UNIQUE,
-    especialidade VARCHAR(100) NOT NULL,
-    logradouro VARCHAR(100) NOT NULL,
-    bairro VARCHAR(100) NOT NULL,
-    cep VARCHAR(9) NOT NULL,
-    complemento VARCHAR(100),
-    numero VARCHAR(20),
-    uf CHAR(2) NOT NULL,
-    cidade VARCHAR(100) NOT NULL
+    login VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL
 );
