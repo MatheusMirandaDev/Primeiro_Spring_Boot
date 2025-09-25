38ca080a31e19cd8ff340031f6e3337803be8255
Matheus Miranda Dev <matheusmiranda.batista@gmail.com>
2025-09-25 20:20:49 -0300

    feat: cria TockenService para geração de tokens JWT

---
+++ src/main/java/med/voll/api/infra/security/TockenService.java
@@ -0,0 +1,32 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.user.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TockenService {
+
+    public String gerarToken(User user){
+        try {
+            var algorithm = Algorithm.HMAC256("123456789");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        }catch (JWTCreationException ex) {
+            throw new RuntimeException("Erroao gerar token JWT", ex);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
